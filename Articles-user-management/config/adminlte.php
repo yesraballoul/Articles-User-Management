db43bff1c5d7373afcc75d9fccae18f47165266e
balloulyesra <balloulyesra@gmail.com>
2023-06-15 10:26:16 +0300

    -installing datatables (as an adminlte plugin). -changing the configuration to use the installed datatables plugin in public folder instead of getting the files from cdn - this configuration is global so the datatables files will be added to all blade templates

--- Articles-user-management/config/adminlte.php
+++ Articles-user-management/config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'AUM',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -421,22 +421,22 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+                    'asset' => true,
+                    'location' => 'vendor\datatables\js\jquery.dataTables.min.js',
                 ],
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
+                    'asset' => true,
+                    'location' => 'vendor\datatables\js\dataTables.bootstrap4.min.js',
                 ],
                 [
                     'type' => 'css',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                    'asset' => true,
+                    'location' => 'vendor\datatables\css\dataTables.bootstrap4.min.css',
                 ],
             ],
         ],
--- Articles-user-management/resources/views/home.blade.php
+++ Articles-user-management/resources/views/home.blade.php
@@ -7,13 +7,108 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div>
+        <p>Welcome to this beautiful admin panel.</p>
+    </div>
+    
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table id="table_id" class="table table-hover">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                                <th>Column 4</th>
+                                <th>Column 5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Row 1 Data 1</td>
+                                <td>Row 1 Data 2</td>
+                                <td>Row 1 Data 3</td>
+                                <td>Row 1 Data 4</td>
+                                <td>Row 1 Data 5</td>
+                            </tr>
+                            <tr>
+                                <td>Row 2 Data 1</td>
+                                <td>Row 2 Data 2</td>
+                                <td>Row 2 Data 3</td>
+                                <td>Row 2 Data 4</td>
+                                <td>Row 2 Data 5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @stop
 
-@section('css')
+{{-- @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+@stop --}}
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script> console.log('Hi!'); 
+        
+        var data = [
+            [
+                "Tiger Nixon",
+                "System Architect",
+                "Edinburgh",
+                "5421",
+                "2011/04/25",
+                "$3,120"
+            ],
+            [
+                "Garrett Winters",
+                "Director",
+                "Edinburgh",
+                "8422",
+                "2011/07/25",
+                "$5,300"
+            ]
+        ]
+        var data2 = [{
+                "name": "Tiger Nixon",
+                "position": "System Architect",
+                "salary": "$3,120",
+                "start_date": "2011/04/25",
+                "office": "Edinburgh",
+                "extn": "5421"
+            },
+            {
+                "name": "Garrett Winters",
+                "position": "Director",
+                "salary": "$5,300",
+                "start_date": "2011/07/25",
+                "office": "Edinburgh",
+                "extn": "8422"
+            }
+        ]
+        $(document).ready(function() {
+            $('#table_id').DataTable({
+                // data: data2,
+                // columns: [{
+                //         data: 'name'
+                //     },
+                //     {
+                //         data: 'position'
+                //     },
+                //     {
+                //         data: 'salary'
+                //     },
+                //     {
+                //         data: 'office'
+                //     }
+                // ]
+            });
+        });
+    </script>
 @stop
